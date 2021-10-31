915df17367f1d76d3cb86b9da5434dfc9f20d45f
Ahmet Ulutaş <englishlectures.stocks@gmail.com>
2021-10-31 18:28:04 +0300

    Gonna publish my landing Page soon. This is the repository I'll be using

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import Typewriter from "typewriter-effect";
 function App() {
   return (
     <div className="App">
       <h1>This is Ahmet Ulutaş's Landing Page</h1>
-      <p>I'm a self-made web developer. Trying to learn React.js on my own. Got some cool projects. Contact me for bussiness cooperation.</p>
+      <p className="type-writer">I'm a <Typewriter 
+    options={{
+    strings: ['Self-Taught Programmer', 'React Student' ,'English Teacher'],
+    autoStart: true,
+    loop: true,
+    delay:200,
+      }}
+      
+      /> </p>
+        
+      <p>Trying to learn React.js on my own. Got some cool projects. Contact me for bussiness cooperation.</p>
       <p>Gonna publish my own landing page here soon.</p>
     </div>
   );
